227d5358ca90fedc238567c99910ff2922f912b6
Baptiste <baptiste.ossipow@gmail.com>
2013-10-09 14:48:56 +0200

    first pull push

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/nfs/http11/hybrid-concept/.virtualenvs/djshop/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lib/python2.6/locale.py
@@ -0,0 +1,1 @@
+/usr/languages/python/2.6/lib/python2.6/locale.py
